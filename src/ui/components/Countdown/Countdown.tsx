e1e7d44fd50473c77b7726244a3e1c1e16d8a485
Kasnix <luc.kasnix@gmail.com>
2022-09-17 10:07:47 -0300

    feat: add Countdown component

---
+++ src/ui/components/Countdown/Countdown.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+import { Palette } from '../../../utils/constants/ui';
+
+interface CountdownProps {
+  minutes?: number;
+  isPaused: boolean;
+  onProgress: (num: number) => void;
+  onEnd: () => void;
+};
+
+const minutesToMillis = (min: number) => min * 1000 * 60;
+const formatTime = (time: number) => (time < 10 ? `0${time}` : time.toString());
+
+export default function Countdown ({ minutes = 0.1, isPaused, onProgress, onEnd }: CountdownProps) {
+  const interval = React.useRef<number>(0);
+  const [millis, setMillis] = React.useState<number>(0);
+
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        clearInterval(interval.current);
+        onEnd();
+        return time;
+      }
+      const timeLeft = time - 1000;
+      return timeLeft;
+    });
+  };
+
+  React.useEffect(() => {
+    setMillis(minutesToMillis(minutes));
+  }, [minutes]);
+
+  React.useEffect(() => {
+    onProgress(millis / minutesToMillis(minutes));
+  }, [millis]);
+
+  React.useEffect(() => {
+    if (isPaused) {
+      if (interval.current) clearInterval(interval.current);
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  return (
+    <Text style={styles.text}>
+      {formatTime(minute)}:{formatTime(seconds)}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: Palette.PrimaryContrastText,
+    padding: 24,
+    backgroundColor: 'rgba(94, 132, 226, 0.3)',
+  },
+});

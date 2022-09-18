1affffa8148ad6a5fc312c8c211b74d367a49da0
Kasnix <luc.kasnix@gmail.com>
2022-09-18 11:00:10 -0300

    feat: add navigation setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { RecoilRoot } from 'recoil';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from './src/ui/screens';
+import { FocusedSubject, Home } from './src/ui/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <RecoilRoot>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="FocusedSubject" component={FocusedSubject} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </RecoilRoot>
   );
 }

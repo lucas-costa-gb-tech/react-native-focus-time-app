ef0908855978c2ac3afbf255827b7d203e1ed0a4
Kasnix <luc.kasnix@gmail.com>
2022-09-20 16:32:52 -0300

    feat: add countdown to FocusedSubject screen

--- src/ui/screens/FocusedSubject/FocusedSubject.tsx
+++ src/ui/screens/FocusedSubject/FocusedSubject.tsx
@@ -1,13 +1,69 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import { Basic } from '../../templates';
+import { Countdown, RoundedButton } from '../../components';
 
 export default function FocusedSubject() {
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+
+  const handleProgress = () => {};
+
+  const handleEnd = () => {};
+
+  const handleStart = () => {
+    setIsStarted(true);
+  };
+
+  const handlePause = () => {
+    setIsStarted(false);
+  };
+
   return (
     <Basic>
-      <View>
-        <Text>Focused Subject Screen</Text>
+      <View style={styles.container}>
+        <View style={styles.countdownContainer}>
+          <Countdown
+            minutes={1}
+            isPaused={!isStarted}
+            onProgress={handleProgress}
+            onEnd={handleEnd}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          {isStarted ? (
+            <RoundedButton
+              size={125}
+              title="pause"
+              onPress={handlePause}
+            />
+          ) : (
+            <RoundedButton
+              size={125}
+              title="start"
+              onPress={handleStart}
+            />
+          )}
+        </View>
       </View>
     </Basic>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  countdownContainer: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 0.3,
+    flexDirection: 'row',
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

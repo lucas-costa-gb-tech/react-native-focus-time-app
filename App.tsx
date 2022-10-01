aebd5de4e05dd337a83e3c81cd9f01a64137851f
Kasnix <luc.kasnix@gmail.com>
2022-10-01 15:36:47 -0300

    feat: add Paper provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { RecoilRoot } from 'recoil';
+import { MD3DarkTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import { Navigator } from './src/navigation';
 
 export default function App() {
   return (
     <RecoilRoot>
-      <Navigator />
+      <PaperProvider theme={MD3DarkTheme}>
+        <Navigator />
+      </PaperProvider>
     </RecoilRoot>
   );
 }
